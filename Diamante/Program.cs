cda419475ebaf3418a484cc898c718bcda0d902e
George Hamann <george.hamann@gmail.com>
2024-04-18 19:03:08 -0300

    resolução

---
+++ Diamante/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Diamante
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tamanho = 21;
+
+            int meio = tamanho / 2;
+
+            // Imprime a parte superior do diamante
+            for (int i = 0; i < meio; i++)
+            {
+                for (int j = 0; j < tamanho; j++)
+                {
+                    if (j >= meio - i && j <= meio + i)
+                    {
+                        Console.Write("X");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            // Imprime a parte inferior do diamante
+            for (int i = meio; i >= 0; i--)
+            {
+                for (int j = 0; j < tamanho; j++)
+                {
+                    if (j >= meio - i && j <= meio + i)
+                    {
+                        Console.Write("X");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
